fb71bc1ce33c4cb52bcc5203378a552fdde13131
dealbertt <dealbertignacio@gmail.com>
2025-05-22 12:18:01 +0200

    We have a window

---
+++ makefile
@@ -0,0 +1,23 @@
+CXX = g++  
+CC = gcc   
+CXXFLAGS = -Wall -Werror -std=c++17 -Iinclude $(shell pkg-config --cflags sdl3) -lSDL3_ttf 
+LDFLAGS = $(shell pkg-config --libs sdl3 sdl3-ttf)   
+
+SRC_DIR = src
+SRC = $(SRC_DIR)/main.cpp 
+
+OBJ_DIR = obj
+OBJ = $(SRC:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o)
+
+TARGET = CSort
+
+$(TARGET): $(OBJ)
+	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJ) $(LDFLAGS)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
+	mkdir -p $(OBJ_DIR)
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+
+clean:
+	rm -rf $(OBJ_DIR) $(TARGET)
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_render.h>
+#include <iostream>
+#include <SDL3/SDL.h>
+
+#define SDL_HINT_NO_SIGNAL_HANDLERS   "SDL_NO_SIGNAL_HANDLERS"
+#define WINDOW_WIDTH 1600 
+#define WINDOW_HEIGHT 900
+
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+int initProgram(){
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        std::cout << "Error trying to initialize SDL" << std::endl;
+        return -1;
+    }
+
+    window = SDL_CreateWindow("CSort", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+    if(window == NULL){
+        std::cout << "Error trying to create SDL_Window" << std::endl;
+        return -1;
+    }
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    std::cout << "Window created successfully!" << std::endl;
+
+    renderer = SDL_CreateRenderer(window, NULL);
+    if(renderer == NULL){
+        std::cout << "Error while trying to create the renderer" << std::endl;
+        return -1;
+        
+    }
+    SDL_RenderClear(renderer);
+    std::cout << "Renderer created successfully!" << std::endl;
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
+    return 0;
+}
+int main(){
+    std::cout << "Hello World" << std::endl; 
+    initProgram();
+
+    return 0;
+}
