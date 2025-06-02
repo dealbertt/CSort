6e89fa37be8de090ac4a20fc0a7f505df3637a70
dealbertt <dealbertignacio@gmail.com>
2025-06-02 11:22:45 +0200

    Dumbest segfault i have ever seen, it is now fixed, still got no sound tho

--- src/array.cpp
+++ src/array.cpp
@@ -17,10 +17,8 @@ void Delay::delay(){
 void ArrayItem::onAccess() const {
     std::cout << "[DEBUG] onAccess called. Value: " << this->getValue() << std::endl;
     std::cout << "[DEBUG] this pointer: " << static_cast<const void*>(this) << std::endl;
-    //std::this_thread::sleep_for(std::chrono::milliseconds(500)); pv
 
-
-    //SoundAccess(this->get());
+    SoundAccess(this->getValue());
 }
 
 void ArrayItem::onComparison(const ArrayItem &item) const {
@@ -28,8 +26,8 @@ void ArrayItem::onComparison(const ArrayItem &item) const {
 
     std::cout << "[DEBUG] onComparison called. Value: " << this->getValue() << std::endl;
     std::cout << "[DEBUG] onComparison second called. Value: " << item.getValue() << std::endl;
-    //SoundAccess(this->getValue());
-    //SoundAccess(item.getValue());
+    SoundAccess(this->getValue());
+    SoundAccess(item.getValue());
 }
 
 void Array::FillArray(){
@@ -38,7 +36,7 @@ void Array::FillArray(){
     std::random_device rd;  // Seed for the random number engine
     std::mt19937 gen(rd()); // Mersenne Twister PRNG
     std::uniform_int_distribution<ArrayItem::valueType> dist(1,  getMaxValue()); // Generates 0 or 1
-                                                                                 //
+                                                                                 
     size_t size = getSize();
 
     for(size_t i = 0; i < size; i++){
